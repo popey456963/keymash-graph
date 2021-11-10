62a399708ccb5188ce785be2243783681f9b81ed
Alexander C <gitlab@femto.host>
2021-11-10 00:42:45 +0000

    update pages

--- pages/api/info.js
+++ pages/api/info.js
@@ -1,8 +1,14 @@
 import fetch from 'node-fetch'
 
 export default async function handler(req, res) {
-  const info = await fetch(`https://api.keyma.sh/api/v2/player/info?name=${req.query.name}`)
-    .then(fetchRes => fetchRes.json())
+  let info
+  try {
+    info = await fetch(`https://api.keyma.sh/api/v2/player/info?name=${req.query.name}`)
+      .then(fetchRes => fetchRes.json())
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
 
   res.status(200).json(info)
 }
